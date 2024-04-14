bf70789f87dbfedfbf29d73d1062d65f0ad81775
Aaron McDonald <163208575+aaron-dm-mcdonald@users.noreply.github.com>
2024-04-14 16:04:44 -0700

    Create hello_world.js

---
+++ hello_world.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 8080;
+
+const msg = 'Hello Team! Welcome to GCP World. I am Anshul & I hope, you all are doing good';
+const fullMsg = `${msg}\n`;
+
+const server = http.createServer((request, response) => {
+    response.statusCode = 200;
+    response.setHeader('Content-Type', 'text/plain');
+    response.end(fullMsg);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Application running at http://${hostname}:${port}/`);
+});
